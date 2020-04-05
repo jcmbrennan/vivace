0c842fd2dc11227082faa11ac030c4ab005c572e
John Brennan <jcbrennan@gmail.com>
2020-04-05 16:00:14 +0100

    Functionality to diplay orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,10 @@ class OrdersController < ApplicationController
   # GET /orders/1
   # GET /orders/1.json
   def show
+    @orderlessons = Orderlesson.all
+    @orderlessons = Orderlesson.where(order_id: params[:id])
+    @user = User.find(current_user.id)
+    @orders = @user.orders.all
   end
 
   # GET /orders/new
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,6 +11,20 @@ class StaticPagesController < ApplicationController
   def about
   end
 
+  def paid
+    # redirect_to "/cart/clear"
+    flash[:notice] = 'Transaction Complete'
+    @order = Order.last
+    @order.update_attribute(:status , "Paid by User: #{current_user.email}")
+    #"Paid by User:#{current_user.id} #{current_user.name} #{current_user.surname}")
+    session[:cart] = nil
+    
+    @orders = Order.all
+    @orderlessons = Orderlesson.all
+    @lessons = Lesson.all
+    
+  end
+
   
   
 end
