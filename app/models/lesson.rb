91e978b92e5a580661dbdc2221f4a3a901457116
John Brennan <jcbrennan@gmail.com>
2020-04-15 22:29:19 +0100

    Updated the lesson model to incorporate defaults for image placeholders

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,10 @@ class Lesson < ApplicationRecord
 	validates_presence_of :title, :price, :category_name, :start_date, :duration, :tutor_name
 
 	belongs_to :category
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.image_url ||= "http://placehold.it/350x200"
+	end
 end 
