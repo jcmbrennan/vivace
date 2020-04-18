165fef209ca2e8802e481b5efea0240251f30509
John Brennan <jcbrennan@gmail.com>
2020-04-18 23:07:52 +0100

    Updated the master stylesheet and created some helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < ActionController::Base
 	include DeviseWhitelist
 	include CurrentUserConcern
 	include DefaultPageContent
+
+	before_action :set_copyright
+	def set_copyright
+		@copyright=VivaceViewTool::Renderer.copyright 'John Brennan', 'All rights reserved'
+	end
+end
+
+module VivaceViewTool
+	class Renderer
+		def self.copyright name, msg
+			"&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+		end
+	end
 end
