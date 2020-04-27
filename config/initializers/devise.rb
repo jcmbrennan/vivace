9bd0d4155791036c6509494b59f0013f981f6b98
John Brennan <jcbrennan@gmail.com>
2020-04-27 19:54:41 +0100

    Changes made to application rb and devise initializer to facilitate upload to Heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -122,6 +122,8 @@ Devise.setup do |config|
   # Send a notification email when the user's password is changed.
   # config.send_password_change_notification = false
 
+  config.secret_key = '-- secret key --'
+
   # ==> Configuration for :confirmable
   # A period that the user is allowed to access the website even without
   # confirming their account. For instance, if set to 2.days, the user will be
